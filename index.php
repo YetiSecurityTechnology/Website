d5a077abd103b19f9f38d116e4690d3dca6d356b
YetiSecurityTechnology <34455452+YetiSecurityTechnology@users.noreply.github.com>
2017-12-11 19:14:05 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+<title>YetiSecurityTechnology Login</title>
+</head>
+
+<h1>Welcome to YetiSecurityTechnology
+
+<form action="login.php">
+
+  <div class="container">
+    <label><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit">Login</button>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+  </div>
+</form>
+
+
+</html
